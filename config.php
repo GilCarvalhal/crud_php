edb76e0c559ffe0424bd8c9f2e8a3c01feef86af
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2024-04-14 12:09:35 -0300

    modificacao no db_user e db_password

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 $db_name = 'crud_php';
 $db_host = 'localhost';
-$db_user = 'adminuser';
-$db_password = 'password';
+$db_user = 'root';
+$db_password = 'root';
 
 $pdo = new PDO("mysql:dbname=" . $db_name . ";host=" . $db_host, $db_user, $db_password);
