b36361ce1a1ea2608eb2b0b1a70b9b0072fadc51
GilCarvalhal <gilbertocarvalhalmatos@gmail.com>
2025-03-26 21:02:56 -0300

    Implementando 'asterisco' para informar ao usuário que é obrigatório o preenchimento do cadastro

--- cadastrar.php
+++ cadastrar.php
@@ -21,22 +21,22 @@
             <form class="box-form" action="cadastrar_action.php" method="post">
                 <div class="label">
                     <label>
-                        <p>Nome Completo:</p>
+                        <p>Nome Completo: <span style="color: red;">*</span></p>
                         <input type="text" name="nome" placeholder="Digite seu nome..." required>
                     </label>
                     <label>
-                        <p>Email:</p>
+                        <p>Email: <span style="color: red;">*</span></p>
                         <input type="email" name="email" placeholder="Digite seu email..." required>
                     </label>
                     <label>
-                        <p>Sexo:</p>
+                        <p>Sexo: <span style="color: red;">*</span></p>
                         <input type="radio" name="sexo" value="M">
                         <p class="m">Masculino</p>
                         <input type="radio" name="sexo" value="F">
                         <p class="f">Feminino</p>
                     </label>
                     <label>
-                        <p>Nascimento:</p>
+                        <p>Nascimento: <span style="color: red;">*</span></p>
                         <input type="date" name="nascimento" required>
                     </label>
                     <label>
@@ -44,19 +44,19 @@
                         <input type="tel" name="telefone" placeholder="xxxxxxxxxx" oninput="validarTelefoneCelular(this)">
                     </label>
                     <label>
-                        <p>Celular:</p>
+                        <p>Celular: <span style="color: red;">*</span></p>
                         <input type="tel" name="celular" placeholder="xxxxxxxxxx" oninput="validarTelefoneCelular(this)" required>
                     </label>
                     <label>
-                        <p>Endereço:</p>
+                        <p>Endereço: <span style="color: red;">*</span></p>
                         <input type="text" name="endereco" placeholder="Digite seu endereço..." required>
                     </label>
                     <label>
-                        <p>Cidade:</p>
+                        <p>Cidade: <span style="color: red;">*</span></p>
                         <input type="text" name="cidade" placeholder="Digite sua região..." required>
                     </label>
                     <label>
-                        <p>UF:</p>
+                        <p>UF: <span style="color: red;">*</span></p>
                         <input type="text" name="uf" placeholder="Digite sua UF..." oninput="validarUF(this)" required>
                     </label>
                 </div>
